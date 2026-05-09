3c8ade39024a4915fca8bde323098683e52121ed
neoToad <colinjmass@gmail.com>
2026-05-09 17:59:54 -0400

    Adding allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ load_dotenv()
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 SHOW_LOCKED_COMPLETED_QUESTS = False
 DEV_ENVIRONMENT = os.getenv("DEV_ENVIRONMENT") == "True"
 
